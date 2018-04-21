73ff51ae7a2c88b611f3a113794ea256da4ca99c
axchemezov <alexchemezov@mail.ru>
2018-04-21 15:40:50 +1000

    Math training from Dmitry Lavrik course is done

---
+++ VuePlusWebPack/src/components/ResultScreen.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="alert alert-primary">
+        <h3>Success: {{stats.success}}</h3>
+        <h3>Error: {{stats.error}}</h3>
+        <hr>
+        <button class="btn btn-primary" @click="$emit('repeat')">Repeat</button>
+        <button class="btn btn-primary"
+                @click="$emit('nextLevel')"
+                v-if="endOfTest"
+        >Next level
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['stats', 'endOfTest']
+    }
+</script>
+
+<style scoped>
+
+</style>
